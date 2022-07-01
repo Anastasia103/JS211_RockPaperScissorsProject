6ffb386efedda67256418bbf1b4e4070587beb2d
Anastasia103 <anastasia.sutherland12@gmail.com>
2022-06-30 22:24:45 -0400

    added HTML and Javascript for rps

--- main.js
+++ main.js
@@ -1,23 +1,50 @@
-// uses strict mode so strings are not coerced, variables are not hoisted, etc... 
-'use strict';
+let hand2 = ""
+let hand1 = ""
 
-// brings in the assert module for unit testing
-const assert = require('assert');
-// brings in the readline module to access the command line
-const readline = require('readline');
-// use the readline module to print out to the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+function updatedHandle1() {
+  hand1 = document.getElementById("handle1").value
 
-// the function that will be called by the unit test below
-const rockPaperScissors = (hand1, hand2) => {
-
-  // Write code here
-  // Use the unit test to see what is expected
-
+  console.log(hand1)
 }
+function updatedHandle2() {
+  hand2 = document.getElementById("handle2").value
+
+  console.log(hand2)
+}
+// the function that will be called by the unit test below
+const rockPaperScissors = () => { 
+let userOne = hand1.toLowerCase().trim()
+let userTwo = hand2.toLowerCase().trim()
+
+
+if (userOne === 'scissors' && userTwo ==='scissors' ||
+    userOne === 'rock' && userTwo ==='rock' || 
+    userOne === 'paper' && userTwo ==='paper')
+    {
+      document.getElementById("whoWins").innerHTML = ("It's a tie!") 
+      console.log("It's a tie!")
+}
+ else if (userOne === 'rock' && userTwo ==='scissors' ||
+          userOne === 'scissors' && userTwo === 'paper' ||
+          userOne === 'paper' && userTwo ==='rock')
+         
+          {
+   document.getElementById("whoWins").innerHTML = ("Hand one wins!")     
+   console.log("Hand one wins!")  
+          }
+  else if (userTwo === 'rock' && userOne ==='scissors' ||
+           userTwo === 'scissors' && userOne === 'paper' ||
+           userTwo === 'paper' && userOne ==='rock')
+          {
+  document.getElementById("whoWins").innerHTML = ("Hand two wins!") 
+  console.log("Hand two wins!")         
+          }
+else {
+  document.getElementById("whoWins").innerHTML = ("Invalid arguements must enter rock, paper or scissors") 
+  console.log("Invalid Arguments")   
+}
+
+};
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
@@ -34,29 +61,4 @@ function getPrompt() {
 // Unit Tests
 // to use them run the command: npm test main.js
 // to close them ctrl + C
-if (typeof describe === 'function') {
 
-  // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built returns the expected output.
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-    });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-    });
-    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-    });
-  });
-} else {
-
-  // always returns ask the user for another input
-  getPrompt();
-
-}
